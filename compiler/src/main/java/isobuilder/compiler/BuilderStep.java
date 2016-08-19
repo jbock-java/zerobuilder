4ee1347548a54073d59f7a054cf3c07d1467c102
h908714124 <noreply@noemail.com>
2016-08-19 19:12:12 +0200

    use return value

--- compiler/src/main/java/isobuilder/compiler/BuilderStep.java
+++ compiler/src/main/java/isobuilder/compiler/BuilderStep.java
@@ -1,20 +1,28 @@
 package isobuilder.compiler;
 
 import com.google.auto.common.BasicAnnotationProcessor;
+import com.google.auto.common.MoreElements;
+import com.google.auto.common.MoreTypes;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.SetMultimap;
+import com.squareup.javapoet.ClassName;
 import isobuilder.Builder;
 
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
-import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeMirror;
 import java.lang.annotation.Annotation;
 import java.util.Set;
 
 import static com.google.auto.common.MoreElements.asType;
+import static com.google.auto.common.MoreTypes.asDeclared;
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Iterables.getFirst;
 import static isobuilder.compiler.BuilderInfo.factoryInfo;
+import static javax.lang.model.element.Modifier.PRIVATE;
+import static javax.lang.model.element.Modifier.STATIC;
 import static javax.lang.model.util.ElementFilter.methodsIn;
 
 public class BuilderStep implements BasicAnnotationProcessor.ProcessingStep {
@@ -34,17 +42,17 @@ public class BuilderStep implements BasicAnnotationProcessor.ProcessingStep {
   public Set<Element> process(SetMultimap<Class<? extends Annotation>, Element> elementsByAnnotation) {
     Set<Element> elements = elementsByAnnotation.get(Builder.class);
     Set<ExecutableElement> methods = methodsIn(elements);
-    checkState(methods.size() == 1, "currently only one method allowed");
-    for (ExecutableElement element : methods) {
+    checkState(methods.size() == 1, "currently only one method per class allowed");
+    ExecutableElement method = getFirst(methods, null);
       try {
-        checkState(element.getModifiers().contains(Modifier.STATIC), "target must be static");
-        checkState(!element.getModifiers().contains(Modifier.PRIVATE), "target cannot be private");
-        TypeElement typeElement = asType(element.getEnclosingElement());
-        factoryGenerator.generate(factoryInfo(typeElement, element.getParameters()));
+        checkState(method.getModifiers().contains(STATIC), "method must be static");
+        checkState(!method.getModifiers().contains(PRIVATE), "method cannot be private");
+        DeclaredType returnType = asDeclared(method.getReturnType());
+        TypeElement typeElement = asType(returnType.asElement());
+        factoryGenerator.generate(factoryInfo(typeElement, method.getParameters()));
       } catch (SourceFileGenerationException e) {
         e.printStackTrace();
       }
-    }
     return ImmutableSet.of();
   }
 
