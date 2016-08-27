eb68655e16f23d80949531f027c8572e822e5230
h908714124 <noreply@noemail.com>
2016-08-27 19:08:59 +0200

    stash

--- compiler/src/main/java/isobuilder/compiler/MyGenerator.java
+++ compiler/src/main/java/isobuilder/compiler/MyGenerator.java
@@ -4,7 +4,7 @@ import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.squareup.javapoet.ClassName;
-import com.squareup.javapoet.CodeBlock;
+import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterSpec;
 import com.squareup.javapoet.TypeSpec;
@@ -25,8 +25,13 @@ import static javax.lang.model.element.Modifier.STATIC;
 
 final class MyGenerator extends SourceFileGenerator<Target> {
 
-  private static final String STEPS = "STEPS";
-  private static final String UPDATER = "UPDATER";
+  private static final String INSTANCE = "INSTANCE";
+
+  private static final String BUILDER_JAVADOC = Joiner.on('\n').join(ImmutableList.of(
+      "The first step of the builder chain that builds {@link $T}.",
+      "All steps of the builder implementation are mutable.",
+      "It is not recommended to use any of the steps more than once.",
+      "@return A mutable builder without any thread safety guarantees.", ""));
 
   MyGenerator(Filer filer, Elements elements, Messager messager) {
     super(filer, elements, messager);
@@ -45,23 +50,36 @@ final class MyGenerator extends SourceFileGenerator<Target> {
   @Override
   Optional<TypeSpec.Builder> write(
       ClassName generatedClassName, Target target) {
-    MethodSpec builderFactory = builderFactory(target);
-    MethodSpec toBuilder = toBuilder(target);
     return Optional.of(classBuilder(generatedClassName)
-        .addMethod(builderFactory)
-        .addMethod(toBuilder)
+        .addField(instance(target))
+        .addField(target.stepsImpl().name(), "updater", PRIVATE, FINAL)
+        .addField(target.updaterImpl().name(), "steps", PRIVATE, FINAL)
+        .addMethod(builderMethod(target))
+        .addMethod(toBuilderMethod(target))
         .addType(buildImpl(target))
         .addType(buildContract(target))
         .addModifiers(PUBLIC, FINAL)
-        .addMethod(constructorBuilder().addModifiers(PRIVATE).build()));
+        .addMethod(constructor());
   }
 
-  private MethodSpec toBuilder(Target target) {
-    String parameterName = downcase(target.goalType().simpleName());
-    MethodSpec.Builder builder = methodBuilder("toBuilder")
+  private MethodSpec constructor(Target target) {
+    return constructorBuilder()
+        .addStatement("this.$L = new $N()", "updater", target.stepsImpl().name())
+        .addStatement("this.$L = new $N()", "steps", target.stepsImpl().name())
+        .addModifiers(PRIVATE)
+        .build();
+  }
+
+  private FieldSpec instance(Target target) {
+    return FieldSpec.builder();
+  }
+
+  private MethodSpec toBuilderMethod(Target target) {
+    String parameterName = downcase(ClassName.get(target.typeElement).simpleName());
+    MethodSpec.Builder builder = methodBuilder("toBuilderMethod")
         .addParameter(ClassName.get(target.typeElement), parameterName);
     String updater = "updater";
-    builder.addStatement("$N = $N.get()", updater, UPDATER);
+    builder.addStatement("$N = $N.get().updater", updater, INSTANCE);
     for (StepSpec stepSpec : target.stepSpecs) {
       // support getters, DFA
       builder.addStatement("$N.$N($T.$N())", updater, stepSpec.argument.getSimpleName(),
@@ -70,18 +88,14 @@ final class MyGenerator extends SourceFileGenerator<Target> {
     builder.addStatement("return $N", updater);
     return builder.addModifiers(PUBLIC, STATIC).build();
   }
-  private MethodSpec builderFactory(Target target) {
+
+  private MethodSpec builderMethod(Target target) {
     StepSpec firstStep = target.stepSpecs.get(0);
     ParameterSpec parameter = firstStep.asParameter();
     return methodBuilder("builder")
         .returns(firstStep.stepName)
-        .addJavadoc(Joiner.on('\n').join(ImmutableList.of(
-            "The first step of the builder chain that builds {@link $T}.",
-            "All steps of the builder implementation are mutable.",
-            "It is not recommended to use any of the steps more than once.",
-            "@return A mutable builder without any thread safety guarantees.", "")),
-            target.goalType())
-        .addStatement("return $N.get()", STEPS, parameter.name)
+        .addJavadoc(BUILDER_JAVADOC, ClassName.get(target.typeElement))
+        .addStatement("return $N.get().steps", INSTANCE, parameter.name)
         .addModifiers(PUBLIC, STATIC)
         .build();
   }
