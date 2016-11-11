843fd229b76aeec1fe02d24d38c6ea27e996d8e3
h908714124 <noreply@noemail.com>
2016-11-11 21:56:50 +0100

    add GenericsImpl details

--- modules/module-generics/src/main/java/net/zerobuilder/modules/generics/GenericsGenerator.java
+++ modules/module-generics/src/main/java/net/zerobuilder/modules/generics/GenericsGenerator.java
@@ -17,7 +17,7 @@ import static net.zerobuilder.modules.generics.GenericsContract.contractType;
 import static net.zerobuilder.modules.generics.GenericsContract.implType;
 import static net.zerobuilder.modules.generics.GenericsContract.stepInterfaces;
 import static net.zerobuilder.modules.generics.GenericsContract.stepTypes;
-import static net.zerobuilder.modules.generics.GenericsImpl.stepImpls;
+import static net.zerobuilder.modules.generics.VarLife.implTypeParams;
 import static net.zerobuilder.modules.generics.VarLife.methodParams;
 import static net.zerobuilder.modules.generics.VarLife.typeParams;
 import static net.zerobuilder.modules.generics.VarLife.varLifes;
@@ -26,21 +26,30 @@ final class GenericsGenerator {
 
   private final List<TypeSpec> stepSpecs;
   private final List<List<TypeVariableName>> methodParams;
+  private final List<List<TypeVariableName>> typeParams;
+  private final List<List<TypeVariableName>> implTypeParams;
   private final SimpleStaticMethodGoalContext goal;
+  private final ClassName implType;
+  private final GenericsImpl impl;
 
   private GenericsGenerator(List<TypeSpec> stepSpecs,
                             List<List<TypeVariableName>> methodParams,
+                            List<List<TypeVariableName>> typeParams,
+                            List<List<TypeVariableName>> implTypeParams,
                             SimpleStaticMethodGoalContext goal) {
     this.stepSpecs = stepSpecs;
     this.methodParams = methodParams;
+    this.typeParams = typeParams;
+    this.implTypeParams = implTypeParams;
     this.goal = goal;
+    this.implType = implType(goal);
+    this.impl = new GenericsImpl(implType);
   }
 
   TypeSpec defineImpl() {
-    ClassName implType = implType(goal);
     return classBuilder(implType)
         .addModifiers(STATIC, FINAL)
-        .addTypes(stepImpls(implType, stepSpecs, methodParams))
+        .addTypes(impl.stepImpls(implType, stepSpecs, methodParams, implTypeParams))
         .addMethod(constructorBuilder()
             .addStatement("throw new $T($S)", UnsupportedOperationException.class, "no instances")
             .addModifiers(PRIVATE)
@@ -62,8 +71,9 @@ final class GenericsGenerator {
   static GenericsGenerator create(SimpleStaticMethodGoalContext goal) {
     List<List<TypeVariableName>> lifes = varLifes(goal.details.typeParameters, stepTypes(goal));
     List<List<TypeVariableName>> typeParams = typeParams(lifes);
+    List<List<TypeVariableName>> implTypeParams = implTypeParams(lifes);
     List<List<TypeVariableName>> methodParams = methodParams(lifes);
     List<TypeSpec> stepSpecs = stepInterfaces(goal, typeParams, methodParams);
-    return new GenericsGenerator(stepSpecs, methodParams, goal);
+    return new GenericsGenerator(stepSpecs, methodParams, typeParams, implTypeParams, goal);
   }
 }
--- modules/module-generics/src/main/java/net/zerobuilder/modules/generics/GenericsImpl.java
+++ modules/module-generics/src/main/java/net/zerobuilder/modules/generics/GenericsImpl.java
@@ -1,6 +1,7 @@
 package net.zerobuilder.modules.generics;
 
 import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterSpec;
 import com.squareup.javapoet.TypeSpec;
@@ -9,21 +10,40 @@ import com.squareup.javapoet.TypeVariableName;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.squareup.javapoet.MethodSpec.methodBuilder;
 import static com.squareup.javapoet.TypeSpec.classBuilder;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static java.util.Collections.nCopies;
+import static java.util.Collections.singletonList;
+import static javax.lang.model.element.Modifier.FINAL;
+import static javax.lang.model.element.Modifier.PRIVATE;
+import static net.zerobuilder.compiler.generate.ZeroUtil.downcase;
+import static net.zerobuilder.compiler.generate.ZeroUtil.joinCodeBlocks;
+import static net.zerobuilder.compiler.generate.ZeroUtil.parameterSpec;
+import static net.zerobuilder.compiler.generate.ZeroUtil.statement;
+import static net.zerobuilder.compiler.generate.ZeroUtil.transform;
 
 final class GenericsImpl {
 
-  static List<TypeSpec> stepImpls(ClassName implType, List<TypeSpec> stepSpecs,
-                                  List<List<TypeVariableName>> methodParams) {
+  private final ClassName impl;
+
+  List<TypeSpec> stepImpls(ClassName implType,
+                           List<TypeSpec> stepSpecs,
+                           List<List<TypeVariableName>> methodParams,
+                           List<List<TypeVariableName>> typeParams) {
     List<TypeSpec> builder = new ArrayList<>(stepSpecs.size());
     builder.addAll(nCopies(stepSpecs.size(), null));
     for (int i = 0; i < stepSpecs.size(); i++) {
       TypeSpec type = stepSpecs.get(i);
       MethodSpec method = type.methodSpecs.get(0);
       ParameterSpec parameter = method.parameters.get(0);
+      List<FieldSpec> fields = fields(stepSpecs, i);
       builder.set(i, classBuilder(implType.nestedClass(type.name + "Impl"))
-          .addMethod(MethodSpec.methodBuilder(method.name)
+          .addFields(fields)
+          .addMethod(createConstructor(fields))
+          .addTypeVariables(typeParams.get(i))
+          .addMethod(methodBuilder(method.name)
               .addParameter(parameter)
               .addTypeVariables(methodParams.get(i))
               .returns(method.returnType)
@@ -33,7 +53,39 @@ final class GenericsImpl {
     return builder;
   }
 
-  private GenericsImpl() {
-    throw new UnsupportedOperationException("no instances");
+  private MethodSpec createConstructor(List<FieldSpec> fields) {
+    List<ParameterSpec> parameters = transform(fields, field -> parameterSpec(field.type, field.name));
+    return MethodSpec.constructorBuilder()
+        .addParameters(parameters)
+        .addCode(parameters.stream().map(parameter -> statement("this.$N = $N", parameter, parameter))
+            .collect(joinCodeBlocks))
+        .addModifiers(PRIVATE)
+        .build();
+  }
+
+  private List<FieldSpec> fields(List<TypeSpec> stepSpecs, int i) {
+    if (i == 0) {
+      return emptyList();
+    }
+    if (i == 1) {
+      return singletonList(parameterField(stepSpecs.get(0)));
+    }
+    TypeSpec previous = stepSpecs.get(i - 1);
+    return asList(
+        FieldSpec.builder(impl.nestedClass(previous.name), downcase(previous.name) + "Impl",
+            PRIVATE, FINAL)
+            .build(),
+        parameterField(previous));
+  }
+
+  private FieldSpec parameterField(TypeSpec type) {
+    MethodSpec method = type.methodSpecs.get(0);
+    ParameterSpec parameter = method.parameters.get(0);
+    return FieldSpec.builder(parameter.type, parameter.name, PRIVATE, FINAL)
+        .build();
+  }
+
+  GenericsImpl(ClassName impl) {
+    this.impl = impl;
   }
 }
