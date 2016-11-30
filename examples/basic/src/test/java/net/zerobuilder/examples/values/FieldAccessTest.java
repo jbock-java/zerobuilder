a6c548f7a3fd43a949e5793c80b1d10a7f72f363
h908714124 <noreply@noemail.com>
2016-11-30 16:10:37 +0100

    update FieldAccessTest

--- examples/basic/src/test/java/net/zerobuilder/examples/values/FieldAccessTest.java
+++ examples/basic/src/test/java/net/zerobuilder/examples/values/FieldAccessTest.java
@@ -1,25 +1,31 @@
 package net.zerobuilder.examples.values;
 
+import net.zerobuilder.examples.values.FieldAccessBuilders.FieldAccessBuilder;
+import net.zerobuilder.examples.values.FieldAccessBuilders.FieldAccessUpdater;
 import org.junit.Test;
 
 import static net.zerobuilder.examples.values.FieldAccessBuilders.fieldAccessBuilder;
 import static net.zerobuilder.examples.values.FieldAccessBuilders.fieldAccessUpdater;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class FieldAccessTest {
 
   @Test
   public void basicTest() {
-    FieldAccess getters = fieldAccessBuilder().length(12).width(10).height(11);
-    assertThat(getters.length, is(12d));
-    assertThat(getters.width, is(10d));
-    assertThat(getters.height, is(11d));
-    getters = fieldAccessUpdater(getters).length(0).done();
-    assertThat(getters.length, is(0d));
-    assertThat(getters.width, is(10d));
-    assertThat(getters.height, is(11d));
+    FieldAccessBuilder.Length builder = fieldAccessBuilder();
+    FieldAccess original = builder.length(12).width(10).height(11);
+    FieldAccessUpdater updater = fieldAccessUpdater(original);
+    FieldAccess updated = updater.length(0).done();
+    assertThat(original.length, is(12d));
+    assertThat(original.width, is(10d));
+    assertThat(original.height, is(11d));
+    assertThat(updated.length, is(0d));
+    assertThat(updated.width, is(10d));
+    assertThat(updated.height, is(11d));
+    // check that caching works
+    assertTrue(builder == fieldAccessBuilder());
+    assertTrue(updater == fieldAccessUpdater(original));
   }
-
-
 }
