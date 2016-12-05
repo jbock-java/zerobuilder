6f8938c73daf91a56e816839bc42671489192ebd
h908714124 <noreply@noemail.com>
2016-12-05 18:34:15 +0100

    change messages

--- compiler/src/main/java/net/zerobuilder/compiler/Messages.java
+++ compiler/src/main/java/net/zerobuilder/compiler/Messages.java
@@ -4,29 +4,27 @@ import com.squareup.javapoet.AnnotationSpec;
 
 import javax.annotation.Generated;
 import javax.lang.model.util.Elements;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import static java.util.Collections.singletonList;
 
 public final class Messages {
 
   public static final class ErrorMessages {
-
-    private static final String POJO_HINT
-        = " If this is not a POJO, try putting the @Goal annotation on a constructor instead.";
 
     public static final String PRIVATE_METHOD =
         "The goal method may not be private.";
 
     public static final String ABSTRACT_CONSTRUCTOR =
         "An abstract class may not have constructor goals." +
-            " Try putting the @Goal annotation on a static \"factory\" method instead.";
+            " Try using a static factory method instead.";
 
     public static final String PRIVATE_TYPE =
-        "The @Builders annotated class may not be private.";
+        "The class may not be private.";
 
     public static final String NESTING_KIND =
-        "The @Builders annotation can only be used on top level and non-private static inner classes.";
+        "The class must be top level or non-private static inner.";
 
     public static final String STEP_OUT_OF_BOUNDS =
         "Step position must be less than the number of arguments.";
@@ -36,34 +34,34 @@ public final class Messages {
 
     public static final String DUPLICATE_GOAL_NAME =
         "There is another goal with this name. " +
-            "Goal name conflicts can be resolved with the @Goal(name = ...) attribute.";
+            "Try using the @GoalName annotation.";
 
     public static final String BEAN_NO_DEFAULT_CONSTRUCTOR =
-        "A non-private default constructor must exist." + POJO_HINT;
+        "A non-private default constructor must exist.";
 
     public static final String BEAN_PRIVATE_CLASS =
-        "This class may not be private." + POJO_HINT;
+        "This class may not be private.";
 
     public static final String BEAN_ABSTRACT_CLASS =
-        "This class may not be abstract." + POJO_HINT;
+        "This class may not be abstract.";
 
     public static final String BEAN_COULD_NOT_FIND_SETTER =
-        "Could not find a setter." + POJO_HINT;
+        "Could not find the setter.";
 
     public static final String MISSING_PROJECTION =
         "Problem while generating an updater: Missing projection.";
 
     public static final String BEAN_IGNORE_AND_STEP =
-        "@BeanIgnore and @Step don't make sense together." + POJO_HINT;
+        "@BeanIgnore and @Step don't make sense together.";
 
     public static final String STEP_ON_SETTER =
-        "The @Step annotation goes on getters, not setters.";
+        "The @BeanStep annotation goes on getters, not setters.";
 
     public static final String IGNORE_ON_SETTER =
         "The @BeanIgnore annotation goes on getters, not setters.";
 
     public static final String BEAN_SUBGOALS =
-        "Bean goals may not have subgoals." + POJO_HINT;
+        "Bean goals may not have subgoals.";
 
     public static final String TYPE_PARAMS_BEAN =
         "Type parameters are not allowed in bean goals.";
@@ -77,9 +75,9 @@ public final class Messages {
 
     public static final String GENERATED_COMMENTS = "https://github.com/h908714124/zerobuilder";
 
-    public static List<AnnotationSpec> generatedAnnotations(Elements elements) {
+    static List<AnnotationSpec> generatedAnnotations(Elements elements) {
       if (elements.getTypeElement("javax.annotation.Generated") != null) {
-        return Arrays.asList(AnnotationSpec.builder(Generated.class)
+        return singletonList(AnnotationSpec.builder(Generated.class)
             .addMember("value", "$S", ZeroProcessor.class.getName())
             .addMember("comments", "$S", GENERATED_COMMENTS)
             .build());
