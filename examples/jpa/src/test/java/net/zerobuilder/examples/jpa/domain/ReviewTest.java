02524318ea6c58073047803ece1c760ed4f9b5cb
h908714124 <urs@trash-mail.com>
2017-04-06 22:00:02 +0200

    fix the build

--- examples/jpa/src/test/java/net/zerobuilder/examples/jpa/domain/ReviewTest.java
+++ examples/jpa/src/test/java/net/zerobuilder/examples/jpa/domain/ReviewTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
