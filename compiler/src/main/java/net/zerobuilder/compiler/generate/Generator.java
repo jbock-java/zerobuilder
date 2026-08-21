950f14aa8f830895270795624c781fd406da9b6b
jbock <jbock-java@gmx.de>
2026-08-21 20:00:45 +0200

    some refactoring, add unused annotations @RecordBuilder

--- compiler/src/main/java/net/zerobuilder/compiler/generate/Generator.java
+++ compiler/src/main/java/net/zerobuilder/compiler/generate/Generator.java
@@ -5,7 +5,6 @@ import com.palantir.javapoet.FieldSpec;
 import com.palantir.javapoet.TypeSpec;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collector;
 import net.zerobuilder.compiler.generate.DtoGeneratorInput.AbstractGoalInput;
 import net.zerobuilder.compiler.generate.DtoGeneratorOutput.BuilderMethod;
@@ -14,7 +13,6 @@ import net.zerobuilder.compiler.generate.DtoModuleOutput.ModuleOutput;
 
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
-import static net.zerobuilder.compiler.generate.DtoGeneratorInput.goalInputCases;
 import static net.zerobuilder.compiler.generate.ZeroUtil.listCollector;
 
 public final class Generator {
@@ -31,23 +29,25 @@ public final class Generator {
       throw new IllegalArgumentException("no input");
     }
     Set<ClassName> generatedType = goals.stream()
-        .map(DtoGeneratorInput.getContext)
+        .map(DtoGeneratorInput::getContext)
         .map(context -> context.generatedType)
         .collect(toSet());
     if (generatedType.size() != 1) {
       throw new IllegalArgumentException("generated type is ambiguous");
     }
     return goals.stream()
-        .filter(hasParameters::apply)
-        .map(process)
+        .filter(Generator::hasParameters)
+        .map(Generator::process)
         .collect(collectOutput(generatedType.iterator().next()));
   }
 
-  private static final Function<AbstractGoalInput, Boolean> hasParameters =
-      goalInputCases(
-          projected -> !projected.description.parameters.isEmpty(),
-          regular -> !regular.description.parameters.isEmpty(),
-          bean -> !bean.description.parameters.isEmpty());
+  static boolean hasParameters(AbstractGoalInput goalInput) {
+    return switch (goalInput) {
+      case DtoGeneratorInput.ProjectedGoalInput projected -> !projected.description.parameters.isEmpty();
+      case DtoGeneratorInput.RegularSimpleGoalInput regular -> !regular.description.parameters.isEmpty();
+      case DtoGeneratorInput.BeanGoalInput bean -> !bean.description.parameters.isEmpty();
+    };
+  }
 
   private static Collector<ModuleOutput, List<ModuleOutput>, GeneratorOutput> collectOutput(ClassName generatedType) {
     return listCollector(tmpOutputs ->
@@ -78,11 +78,13 @@ public final class Generator {
         .toList();
   }
 
-  private static final Function<AbstractGoalInput, ModuleOutput> process =
-      goalInputCases(
-          projected -> projected.module.process(projected.description),
-          regularSimple -> regularSimple.module.process(regularSimple.description),
-          bean -> bean.module.process(bean.description));
+  private static ModuleOutput process(AbstractGoalInput goalInput) {
+    return switch (goalInput) {
+      case DtoGeneratorInput.ProjectedGoalInput projected -> projected.module.process(projected.description);
+      case DtoGeneratorInput.RegularSimpleGoalInput regular -> regular.module.process(regular.description);
+      case DtoGeneratorInput.BeanGoalInput bean -> bean.module.process(bean.description);
+    };
+  }
 
 
   private Generator() {
